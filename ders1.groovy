c207b91e6f18732ad6dbc2a1d737688293657733
bekirbedir <bekirbedir24@gmail.com>
2017-05-04 13:26:25 +0300

    Add files via upload

---
+++ ders1.groovy
@@ -0,0 +1,35 @@
+
+class GroovyDemoMain {
+
+	static main(args) {
+	
+	def tags =['pazartesi','sali','carsamba','persembe','cuma','cumartesi']
+	println "Dizi Boyutu ${tags.size()} "
+	if(tags[0]=='pazartesi')
+	println "1.gün  ${tags[0]}"
+		
+	tags[6] = 'pazar'
+	println "Dizi Boyutu ${tags.size()} "
+//	println "8.gün  ${tags[7]}"
+//	println "7.gün  ${tags[6]}"
+//	println "Dizi Boyutu ${tags.size()} "
+	
+	for (gun  in tags)
+	{ 
+		if(gun=='cumartesi' || gun == 'pazar')
+		println gun + ":)"
+		
+		else
+		println gun
+	
+		}
+	println "--------------------------------------------"
+	
+	def js =[1:'ilkbahar',2:'sonbahar',3:'yaz']
+	println "nach ${js[1]} kommt ${js[2]}"
+	
+	
+	}
+
+
+}
